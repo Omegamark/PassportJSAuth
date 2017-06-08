d461ff59b77113ea8c3ed901239d99f2b847227a
Omegamark <grant.mark@gmail.com>
2017-06-07 18:30:56 -0600

    mongo database now connected to ejs form

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 // config/database.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/introToMongoDB');
+
 module.exports = {
 
-  'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  'url' : 'mongodb://localhost:27017/introToMongoDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
